e112fb131620664bb765d3c06e2fda91b678b504
Andrew Goodwin <andrew.goodwin@conwaycorp.com>
2019-06-18 13:45:47 -0500

    Initial Commit

---
+++ Singularize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Singularize
+//
+//  Created by Andrew Goodwin on 6/18/19.
+//  Copyright © 2019 DevDrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
